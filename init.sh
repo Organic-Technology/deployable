51bff267d47106f18e65287abe2f58a8eb816aa3
Kristin R. Hethcox <khethcox@happycoin.gq>
2021-04-12 14:02:31 +0200

    iterates !

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo "initial git config:"
 cat .git/config
 
 # one time deal !
-if [ ! -e _site ]; then
+if ! git remote -v | grep -q blockring; then
 echo cloning from dgit ...
 git clone https://ipfs.blockringtm.ml/ipns/QmWZ2uR4NHLZNwgZVqAXK1UpLiueFdz632X1CgYKXrgxZR/d/dr/drit/websites/noticeable.gq.git
 if [ -e noticeable.gq/.git ]; then
@@ -14,15 +14,16 @@ mv .git git
 mv -n noticeable.gq/.git .
 fi
 #mv -n noticeable.gq/* .
-#git checkout .
+git rev-parse --short HEAD
+git checkout .
+git status
 mkdir _site
 cp -p index.htm _site/index.htm
 
 else
+ git checkout .
  echo site already initialized !
 fi
-
-cat .git/config
 ls -l
 
 

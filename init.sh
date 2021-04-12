40c3a92b20260337d77d5a5e85c6fb0356f6d334
Pablo L. Ruckel <pruckel@gctm.gq>
2021-04-12 14:30:17 +0200

    testing ...

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ echo "init: start"
 echo "initial git config:"
 cat .git/config
 
+set -x
 # one time deal !
 if ! git remote -v | grep -q blockring; then
 echo cloning from dgit ...
@@ -21,10 +22,12 @@ mkdir _site
 cp -p index.htm _site/index.htm
 
 else
+ echo site already initialized !
+ git fetch origin master
  git log -1
  git checkout .
- git fetch origin
- echo site already initialized !
+ git pull
+ git log -1
 fi
 ls -l
 
